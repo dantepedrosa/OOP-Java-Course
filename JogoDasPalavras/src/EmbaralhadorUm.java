926e7fc229cdb2a74acb1f437d6fe226dbc76289
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-13 20:43:33 -0300

    Criado classes embaralhadoras

---
+++ JogoDasPalavras/src/EmbaralhadorUm.java
@@ -0,0 +1,10 @@
+
+public class EmbaralhadorUm implements Embaralhador {
+
+	@Override
+	public String embaralhaPalavra(String palavra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
