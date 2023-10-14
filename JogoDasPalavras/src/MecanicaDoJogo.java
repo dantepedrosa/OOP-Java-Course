7be7057a7c77cea632137ab9d55f1a520688db55
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-13 21:18:35 -0300

    Primeira implementação da interface MecanicaDoJogo

--- JogoDasPalavras/src/MecanicaDoJogo.java
+++ JogoDasPalavras/src/MecanicaDoJogo.java
@@ -4,4 +4,14 @@
  */
 public interface MecanicaDoJogo {
 
+	public int incrementaVidas(int v);	// Soma o número de vidas em um e retorna o número atual
+	public int getVidas();				// Retorna o numero de vidas
+	public int getPontos();				// Retorna a quantidade de pontos do jogo atual
+	public boolean getDificuldade(); 	// Determina o tamanho da palavra
+	
+	
+	public String getPalavra();					// Retorna uma palavra embaralhada
+	public boolean enviarTentativa(String p); 	// Enviar a palavra montada. Retorna true para correto.
+	
+	
 }
